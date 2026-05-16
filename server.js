f0de5a1c298ff51ab6170cc46664e7ae5db19f51
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-05-16 16:10:24 +0500

    connect database

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
+require('dotenv').config()
 const app = require('./src/app')
+const connectDb = require('./src/db/db.js')
+
+connectDb()
+
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000")
 })
---
+++ src/db/db.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const connectDb = async ()=>{
+    try {
+        await mongoose.connect(process.env.MONGO_URI)
+        console.log("Database connected Successfully !")
+    } catch (error) {
+        console.log("Database connection FAILED !", error)
+    }
+}
+
+module.exports = connectDb
