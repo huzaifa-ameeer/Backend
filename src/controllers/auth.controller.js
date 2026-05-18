66906b396ca52c7ca2170215f69f030181e7bc5c
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-05-18 22:26:24 +0500

    add enhanced authentication

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,19 +1,37 @@
-const userModel = require('../models/user.model')
-const jwt = require('jsonwebtoken')
+const userModel = require("../models/user.model");
+const jwt = require("jsonwebtoken");
 
-const registerUser = async (req, res)=>{
-    const {username, email, password} = req.body
-    const user = await userModel.create({
-        username, email, password
+const registerUser = async (req, res) => {
+  const { username, email, password } = req.body;
+
+    const userExist = await userModel.findOne({
+        email, username
     })
-    const token = jwt.sign({
-        id: user._id
-    }, process.env.JWT_SECRET)
-    res.status(201).json({
-        message: "User created successfully",
-        user,
-        token
-    })
-}
- 
-module.exports = {registerUser}
+
+    if(userExist) {
+        return res.status(409).json({
+            message: "User already exist"
+        })
+    }
+
+  const user = await userModel.create({
+    username,
+    email,
+    password,
+  });
+  const token = jwt.sign(
+    {
+      id: user._id,
+    },
+    process.env.JWT_SECRET,
+  );
+
+  res.cookie("token", token);
+
+  res.status(201).json({
+    message: "User created successfully",
+    user,
+  });
+};
+
+module.exports = { registerUser };
--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -4,5 +4,12 @@ const authController = require('../controllers/auth.controller')
 const router = express.Router()
 
 router.post('/register', authController.registerUser)
+router.get('/test', (req, res)=>{
+    console.log(req.cookies)
+    res.status(200).json({
+        message: "test route",
+        cookies: req.cookies
+    })
+})
 
 module.exports = router
